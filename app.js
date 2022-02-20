d6cc9a12c7e00eaed990a72db0f576d12e05b6a1
Tounyyyyy <tonyluu125576@gmail.com>
2022-02-20 13:15:15 +0100

    Fix Hamburger menu

--- app.js
+++ app.js
@@ -9,18 +9,3 @@ hamburger.addEventListener("click", () => {
   });
 });
 
-<script src="https://smtpjs.com/v3/smtp.js">
-</script>
-    function sendEmail(){
-      Email.send({
-          Host : "smtp.gmail.com",
-          Username : "tonyluu125576@gmail.com",
-          Password : "pt030506",
-          To : 'luutony82@gmail.com',
-          From : "tonyluu125576@gmail.com",
-          Subject : "New Contact Form Enquiry",
-          Body : "And this is the body"
-      }).then(
-      message => alert(message)
-      );
-  } 
